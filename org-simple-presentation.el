42194ef2ee745ee068b0a1ae015a915a741c52ea
takaishi <r_takaishi@eiliant.com>
2011-04-07 22:47:52 +0900

    マイナーモードとして動作するようにした

--- org-simple-presentation.el
+++ org-simple-presentation.el
@@ -2,10 +2,45 @@
 ;; If you want to create a file, visit that file with C-x C-f,
 ;; then enter the text in that file's own buffer.
 
-(global-set-key (kbd "M-n") 'org-presentation-goto-next)
-(global-set-key (kbd "M-p") 'org-presentation-goto-prebious)
+(defvar org-simple-presentation-mode nil)
+(defvar org-simple-presentation-mode-map nil)
 
-(defun org-presentation-start ()
+(if (not (assq 'org-simple-presentation-mode minor-mode-alist))
+    (setq minor-mode-alist
+          (cons '(org-simple-presentation-mode "Org Simple Presentation")
+                minor-mode-alist)))
+
+(defun org-simple-presentation-define-mode-map ()
+  "キーマップ `skk-j-mode-map' を定義する。"
+  (unless (keymapp org-simple-presentation-mode-map)
+    (setq org-simple-presentation-mode-map (make-sparse-keymap))
+    (setq minor-mode-map-alist
+          (cons (cons 'org-simple-presentation-mode org-simple-presentation-mode-map)
+                minor-mode-map-alist))))
+
+;; (unless (assq 'org-simple-presentation-mode minor-mode-map-alist)
+;;   (setq minor-mode-map-alist
+;;         (cons (cons 'org-simple-presentation-mode org-simple-presentation-mode-map)
+;;               (minor-mode-map-alist))))
+
+
+
+(defun org-simple-presentation-mode (&optional arg)
+  "Org Simple Presentation minor-mode"
+  (interactive)
+  (cond
+   ((< (prefix-numeric-value arg) 0)
+    (setq org-simple-presentation-mode nil))
+   (arg
+    (setq org-simple-presentation-mode t))
+   (t
+    (setq org-simple-presentation-mode (not org-simple-presentation-mode))))
+  (if org-simple-presentation-mode
+      (org-simple-presentation-define-mode-map)
+    nil))
+
+
+(defun org-simple-presentation-start ()
   (interactive)
   (let ((cbuf (current-buffer))
 	(cwin (selected-window))
@@ -22,14 +57,14 @@
       (overlay-put (make-overlay (point-min) beg) 'invisible 'spec)
       (overlay-put (make-overlay (+ 1 end) (point-max)) 'invisible 'spec)))
 
-(defun org-presentation-stop ()
+(defun org-simple-presentation-stop ()
   (interactive)
   (let ((ol (overlays-in (point-min) (point-max))))
     (mapcar '(lambda (x)
                (if (overlay-get x 'invisible)
                    (delete-overlay x))) ol)))
 
-(defun org-presentation-goto-next ()
+(defun org-simple-presentation-goto-next ()
   (interactive)
   (let ((cbuf (current-buffer))
 	(cwin (selected-window))
@@ -46,7 +81,7 @@
     (overlay-put (make-overlay (point-min) beg) 'invisible 'spec)
     (overlay-put (make-overlay (+ 1 end) (point-max)) 'invisible 'spec)))
 
-(defun org-presentation-goto-prebious ()
+(defun org-simple-presentation-goto-prebious ()
   (interactive)
   (let ((cbuf (current-buffer))
 	(cwin (selected-window))
@@ -63,3 +98,10 @@
     (org-presentation-stop)
     (overlay-put (make-overlay (point-min) beg) 'invisible 'spec)
     (overlay-put (make-overlay (+ 1 end) (point-max)) 'invisible 'spec)))
+
+(global-set-key (kbd "M-n") 'org-presentation-goto-next)
+(global-set-key (kbd "M-p") 'org-presentation-goto-prebious)
+(define-key org-simple-presentation-mode-map "C-cn" 'org-simple-presentation-goto-next)
+(define-key org-simple-presentation-mode-map "C-cp" 'org-simple-presentation-goto-previous)
+
+(provide 'org-simple-presentation-mode)
